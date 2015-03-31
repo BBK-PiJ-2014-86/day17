9c86e39e5239b5b2b57be82993133e9fde19df96
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2015-03-31 20:25:10 +0100

    code in main - for loop

--- Exercise4.java
+++ Exercise4.java
@@ -1,9 +1,22 @@
 package day17;
 
+import java.util.Scanner;
+
 public class Exercise4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		Scanner sc = new Scanner (System.in);
+		
+		for (int i = 0; i<10; i++) {
+		
+		System.out.println("Enter the duration (in ms) of task " + i);
+		int in = sc.nextInt();
+		
+		Thread t = new Thread(new ResponsiveUI(in));
+		t.start();
+		
+		}
 
 	}
 
